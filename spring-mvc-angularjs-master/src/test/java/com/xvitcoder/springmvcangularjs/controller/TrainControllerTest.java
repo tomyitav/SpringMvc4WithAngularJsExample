768bd1da35df7bf8835c4d503f123cc95f4a5838
Tom <tomyitav@gmail.com>
2016-10-26 21:46:57 +0300

    slight improve of controller test

--- spring-mvc-angularjs-master/src/test/java/com/xvitcoder/springmvcangularjs/controller/TrainControllerTest.java
+++ spring-mvc-angularjs-master/src/test/java/com/xvitcoder/springmvcangularjs/controller/TrainControllerTest.java
@@ -1,7 +1,9 @@
 package com.xvitcoder.springmvcangularjs.controller;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -11,6 +13,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,6 +43,13 @@ public class TrainControllerTest {
 		Train train1 = new Train("111", "TestTrain1", 200, true);
 		Train train2 = new Train("222", "TestTrain2", 300, true);
 		Mockito.when(trainServiceMock.getAllTrains()).thenReturn(Arrays.asList(train1, train2));
+		Mockito.doAnswer(new Answer<Void>() {
+
+			@Override
+			public Void answer(InvocationOnMock invocation) throws Throwable {
+				return null;
+			}
+		}).when(trainServiceMock).deleteTrainById(Mockito.anyString());
 //		setup mockMvc
 		mockMvc = MockMvcBuilders.standaloneSetup(this.trainController).build();
 	}
@@ -46,7 +57,20 @@ public class TrainControllerTest {
 	@Test
 	public void testGetTrainList() throws Exception {
 		mockMvc.perform(get("/trains/trainslist.json")).andExpect(status().isOk())
-		.andExpect(jsonPath("$[0].id", is("111")));
+		.andExpect(jsonPath("$", hasSize(2)))
+		.andExpect(jsonPath("$[0].id", is("111")))
+		.andExpect(jsonPath("$[0].name", is("TestTrain1")));
+		
+		Mockito.verify(trainServiceMock, Mockito.times(1)).getAllTrains();
+		Mockito.verifyNoMoreInteractions(trainServiceMock);
+	}
+	
+	@Test
+	public void testRemoveTrainById() throws Exception {
+		mockMvc.perform(delete("/trains/removeTrain/111")).andExpect(status().isOk());
+		
+		Mockito.verify(trainServiceMock, Mockito.times(1)).deleteTrainById("111");
+		Mockito.verifyNoMoreInteractions(trainServiceMock);
 	}
 
 	
